f86ca1bcc5b252924d8a99a4b3a7289e7d4ac0bc
Jun Lin <linjunpop@gmail.com>
2012-02-06 20:06:22 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResqueMailhopperExample::Application.config.secret_token = '9f305e009c79e7f3b934848c7ce411a7ae6579442cc85795e267ac08010cabf6a80a9696be4da8e715d56ec2170fd81492d2a478fee50beb4842858f68bbbc8b'
